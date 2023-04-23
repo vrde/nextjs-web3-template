6481a8bd55a08b51be17bf771899fe693c7945f0
vrde <agranzot@mailbox.org>
2023-04-23 16:49:48 +0200

    Format everything

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import ConnectWallet from "@/components/ConnectWallet";
-import Head from "next/head";
 import styles from "@/styles/Base.module.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>{process.env.NEXT_PUBLIC_TITLE}</title>
-        <meta
-          name="description"
-          content={process.env.NEXT_PUBLIC_DESCRIPTION}
-        />
+        <meta name="description" content={process.env.NEXT_PUBLIC_DESCRIPTION} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
